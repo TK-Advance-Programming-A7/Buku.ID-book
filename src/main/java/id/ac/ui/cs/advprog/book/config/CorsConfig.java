e3b6e74d28d6c1752df0f54d6a9500f56e296460
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-05-26 19:44:55 +0700

    feat: mencoba cors

---
+++ src/main/java/id/ac/ui/cs/advprog/book/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.advprog.book.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public CorsFilter corsFilter() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://bukuid-adpro-a7.up.railway.app"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With",
+        "Access-Control-Request-Method", "Access-Control-Request-Headers", "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Credentials", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return new CorsFilter(source);
+  }
+}
